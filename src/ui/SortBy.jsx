dcdf922199d815f2e71fbf747f182232da2d9648
ronik297 <ronik.allen@gmail.com>
2025-03-01 20:52:48 +0530

    added sorting and filtering of cabins

---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Select from "./Select";
+import { useSearchParams } from "react-router-dom";
+
+export default function SortBy({ options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || options[0].value;
+
+  function handleChange(e) {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      type="white"
+      onChange={handleChange}
+    />
+  );
+}
