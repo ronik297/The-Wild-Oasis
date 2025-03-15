83e5bc1553c84fdfb7ad4b0357e66da7fd7a883e
ronik297 <ronik.allen@gmail.com>
2025-03-15 12:26:44 +0530

    completed login feature end to end

---
+++ src/ui/ProtectedRoutes.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--color-grey-50);
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { isLoading, isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, isLoading]);
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
